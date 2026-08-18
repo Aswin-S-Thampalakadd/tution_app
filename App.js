7cd962496cabaa2e493bdf75e4557df2a44a7a3d
AswinST6477 <aswins.dev6477@gmail.com>
2026-08-18 19:11:18 +0530

    new change on bottm nav

--- App.js
+++ App.js
@@ -222,7 +222,7 @@ function MainTabs() {
         name="Profile"
         component={ProfileStack}
         options={{
-          tabBarLabel: "Profilee",
+          tabBarLabel: "Profile",
         }}
       />
     </Tab.Navigator>
